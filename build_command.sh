850a624663474f1039dc972e6f6555999a12c574
John Di Giorgio <johnrdigiorgio@gmail.com>
2023-07-26 08:31:04 -0700

    Create files and compose structure for including apps (hrms)

---
+++ build_command.sh
@@ -0,0 +1,13 @@
+# Command to build containers. This is how additional apps and modules are incorporated into the images.
+
+export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
+. ./.env
+
+docker build \
+  --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
+  --build-arg=FRAPPE_BRANCH=version-14 \
+  --build-arg=PYTHON_VERSION=3.10.12 \
+  --build-arg=NODE_VERSION=16.20.1 \
+  --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --tag=desiredeffect/erpnext-hrms:$ERPNEXT_VERSION \
+  --file=images/custom/Containerfile .
