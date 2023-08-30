3b0d3b71e7e6ae17efc7c61dcaf6fc0432e8d298
John Di Giorgio <john@desiredeffect.com>
2023-08-29 17:42:07 -0700

    Update versions, python, erp, node

--- build_command.sh
+++ build_command.sh
@@ -6,8 +6,8 @@ export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
 docker build \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-14 \
-  --build-arg=PYTHON_VERSION=3.10.12 \
-  --build-arg=NODE_VERSION=16.20.1 \
+  --build-arg=PYTHON_VERSION=3.11.5 \
+  --build-arg=NODE_VERSION=18.17.1 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
   --tag=desiredeffect/erpnext-hrms:$ERPNEXT_VERSION \
   --file=images/custom/Containerfile .
