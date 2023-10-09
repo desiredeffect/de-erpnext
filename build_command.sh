69f4d24692ef9bdab35109c4e86957d506669ad8
John Di Giorgio <john@desiredeffect.com>
2023-10-09 12:43:04 -0700

    Add de_macrs to custom image

--- build_command.sh
+++ build_command.sh
@@ -4,10 +4,11 @@ export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
 . ./.env
 
 docker build \
+  #--no-cache \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-14 \
   --build-arg=PYTHON_VERSION=3.11.5 \
   --build-arg=NODE_VERSION=18.17.1 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  --tag=desiredeffect/erpnext-hrms:$ERPNEXT_VERSION \
+  --tag=desiredeffect/erpnext-de:$ERPNEXT_VERSION \
   --file=images/custom/Containerfile .
