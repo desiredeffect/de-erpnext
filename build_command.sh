5ec887c5f37b00152a1a2cc45fb48d6afe3976a6
Bretterbear <brettney40hands@gmail.com>
2023-10-28 15:39:26 -0700

    Git token hidden from public folders
    Quick & dirty hiding of personal access token in .env file.
    Note that there may be a security issue here long term
    Apparently docker may store PATS in logs without special handling.
    That'll be a thing to investigate later.

--- build_command.sh
+++ build_command.sh
@@ -1,10 +1,14 @@
 # Command to build containers. This is how additional apps and modules are incorporated into the images.
 
-export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
+# Load in the .env file to grab our token
 . ./.env
 
+# Run a replacement on the token title with SED & export as a base64 var
+json_contents=$(cat ./apps.json)
+json_contents=$(echo "$json_contents" | sed "s/\${DE_MACRS_PAT}/$DE_MACRS_PAT/g")
+export APPS_JSON_BASE64=$(echo $json_contents | base64 -w 0)
+
 docker build \
-  #--no-cache \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-14 \
   --build-arg=PYTHON_VERSION=3.11.5 \
@@ -12,3 +16,5 @@ docker build \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
   --tag=desiredeffect/erpnext-de:$ERPNEXT_VERSION \
   --file=images/custom/Containerfile .
+
+  #--no-cache \
